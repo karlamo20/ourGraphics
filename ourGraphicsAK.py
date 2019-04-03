4631e5afc2320322ba3bd5000a9c445a76b73cd7
csPython <cspython@AD25TF04NGF1J-32.local>
2019-04-03 12:54:49 -0400

    removed outlines and attempted puffs without outlines

--- ourGraphicsAK.py
+++ ourGraphicsAK.py
@@ -1,46 +1,48 @@
+#This code will make a beach with different types of functions
 
 from graphics import *
+
+##def puff_draw (
+    
 
 kWin = GraphWin("Beach", 600, 600)
 kWin.setCoords(0, 0, 600, 600)
 
 back = Rectangle(Point(0, 0), Point(600,600))
 back.setFill(color_rgb(254, 205, 159))
+back.setOutline(color_rgb(254, 205, 159))
 back.draw(kWin)
 
 sun = Circle(Point(300, 350), 150)
 sun.setFill(color_rgb(253, 179, 109))
+sun.setOutline(color_rgb(253, 179, 109))
 sun.draw(kWin)
 
 wSand = Rectangle(Point(0, 0), Point(600, 100))
 wSand.setFill(color_rgb(255, 255, 204))
+wSand.setOutline(color_rgb(255, 255, 204))
 wSand.draw(kWin)
 
 bSand = Rectangle(Point(0, 100), Point(600, 200))
 bSand.setFill(color_rgb(194, 178, 128))
+bSand.setOutline(color_rgb(194, 178, 128))
 bSand.draw(kWin)
 
 ocean = Rectangle(Point(600, 200), Point(0, 400))
 ocean.setFill(color_rgb(0, 119, 190))
+ocean.setOutline(color_rgb(0, 119, 190))
 ocean.draw(kWin)
 
 #clouds
 
-cloud = Circle(Point(100,560), 20)
-cloud.setFill(color_rgb(245, 245, 245))
-cloud.draw(kWin)
+puff = Circle(Point(0,560), 20)
+puff.setFill(color_rgb(245, 245, 245))
+puff.setOutline(Color_rgb(245, 245, 245))
+puff.draw(kWin)
 
-clouds = Circle(Point(100,570), 20)
-clouds.setFill(color_rgb(245, 245, 245))
-clouds.draw(kWin)
 
-cloudss = Circle(Point(110, 575), 20)
-cloudss.setFill(color_rgb(245, 245, 245))
-cloudss.draw(kWin)
 
-cloudsss = Circle(Point(115, 575), 20)
-cloudsss.setFill(color_rgb(245, 245, 245))
-cloudsss.draw(kWin)
+
 
 kWin.getMouse()
 kWin.close()
